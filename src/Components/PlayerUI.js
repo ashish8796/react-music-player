52e039f20807000f0791d38c1fa20608c3a17a04
ashish8796 <ashishsaini.ak@gmail.com>
2020-07-09 14:21:20 +0530

    Player UI css completed

--- src/Components/PlayerUI.js
+++ src/Components/PlayerUI.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay, faPause, faCaretLeft, faCaretRight } from "@fortawesome/free-solid-svg-icons";
 
 export function PlayerUI() {
   const [songControl, setSongControl] = useState(true);
@@ -26,7 +28,6 @@ export function PlayerUI() {
 
         <article className="song-pic">
           <div className="circle">
-            {/* <img src="./../image/matheo-jbt-Y8QmGCl8lLk-unsplash.jpg" alt="song image" /> */}
           </div>
         </article>
 
@@ -42,20 +43,37 @@ export function PlayerUI() {
             <p className="increasing-time">1:05</p>
             <p className="decreasing-time">2:40</p>
           </div>
-          <div className="indicator"></div>
+          <div className="indicator">
+            <div className="inside"></div>
+          </div>
           <div className="progress-bar"></div>
         </article>
 
         <article className="player-controls">
-          <button className="previous-song">
-            &#x025C2;&#x025C2;
-          </button>
-          <button className="paly-pause" onClick={() => {
-            setSongControl(!songControl);
-          }}>
-            {songControl ? <p>&#x025B8;</p> : <p>&#x0003D;</p>}
-          </button>
-          <button className="next-song">&#x025B8;&#x025B8;</button>
+          <div className="previous button-cover">
+            <button className="previous-song">
+              <p>
+                <FontAwesomeIcon icon={faCaretLeft} />
+                <FontAwesomeIcon icon={faCaretLeft} />
+              </p>
+            </button>
+          </div>
+          <div className="play button-cover">
+            <button className="play-pause" onClick={() => {
+              setSongControl(!songControl);
+            }}>
+              {songControl ? <p><FontAwesomeIcon icon={faPlay} /></p> : <p><FontAwesomeIcon icon={faPause} /></p>}
+            </button>
+          </div>
+          <div className="next button-cover">
+            <button className="next-song">
+              <p>
+                <FontAwesomeIcon icon={faCaretRight} />
+                <FontAwesomeIcon icon={faCaretRight} />
+              </p>
+            </button>
+          </div>
+
         </article>
 
       </div>
