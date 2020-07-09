0a975660c5a4fc63c7bf44616370282ebe05e01a
ashish8796 <ashishsaini.ak@gmail.com>
2020-07-09 16:45:31 +0530

    Queue UI css completed

--- src/Components/Queue.js
+++ src/Components/Queue.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart, faEllipsisH } from "@fortawesome/free-solid-svg-icons";
+import { faHeart, faEllipsisH, faPlay, faPause } from "@fortawesome/free-solid-svg-icons";
 
 export default function Queue() {
+  const [regulateSong, setRegulateSong] = useState(true);
+
   return (
     <div className="queue">
       <section className="sect-1">
@@ -27,13 +29,17 @@ export default function Queue() {
       </section>
       <section className="sect-2">
         <article className="songs-list">
-          <div className="song">
+          <div className={`song ${!regulateSong && "song-playing"}`}>
             <div className="song-info">
               <p>Ain't No Time</p>
               <p>Future</p>
             </div>
-            <div className="btn-shell">
-              <button className="song-btn"></button>
+            <div className={`btn-shell ${!regulateSong && "playing"}`} style={{ boxShadow: regulateSong ? "-4px -4px 5px 4px #3c4348" : "-4px -4px 5px 4px #23272a" }}>
+              <button className='song-btn' onClick={() => {
+                setRegulateSong(!regulateSong);
+              }}>
+                {regulateSong ? <p style={{ marginLeft: "4px" }}><FontAwesomeIcon icon={faPlay} /></p> : <p><FontAwesomeIcon icon={faPause} /></p>}
+              </button>
             </div>
           </div>
         </article>
